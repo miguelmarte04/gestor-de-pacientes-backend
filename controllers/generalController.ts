3e55d88831978207c1bddb78ac27314990d70b35
Miguel Marte <mjml0412@gmail.com>
2023-02-11 20:47:45 -0400

    agregando cambios

--- controllers/generalController.ts
+++ controllers/generalController.ts
@@ -1,73 +1,8 @@
 import { Request, Response } from 'express'
-import fetch from 'node-fetch'
-import { DbError, unirArrayEnObjeto } from '../constants/general'
-import { Personas, provincias, parametros, Consultas } from '../entity/general'
+import { Consultas } from '../entity/general'
 const conexion = require('../database/db')
 import { errorData } from '../constants/general'
-import { Empresa } from '../entity/auth'
 export type AnyType<T = any> = T
-
-exports.getPersonas = async (req: Request, res: Response) => {
-  const { doc_identidad } = await new Personas(req.body.condition)
-  try {
-    const response = await fetch(process.env.GET_PERSON_DATA + doc_identidad)
-    const { data } = await response.json()
-    if (data) {
-      res.status(200).send({ data })
-    } else {
-      res.status(400).send(DbError('Persona no encontrada'))
-    }
-  } catch (error: AnyType) {
-    res.status(400).send({ message: error })
-  }
-}
-exports.getPersona = async (req: Request, res: Response) => {
-  const { doc_identidad } = await new Personas(req.body.condition)
-  conexion.query(
-    'SELECT * FROM empleados where doc_identidad = ?',
-    [doc_identidad],
-    (err: AnyType, results: AnyType) => {
-      if (results?.length === 0) {
-        res.status(400).send({ message: errorData })
-      } else {
-        res.status(200).send({ message: 'Existe una persona' })
-      }
-    }
-  )
-}
-
-exports.getCivilState = async (req: Request, res: Response) => {
-  try {
-    conexion.query(
-      'SELECT * FROM estado_civil',
-      (err: AnyType, results: AnyType) => {
-        if (results?.length === 0) {
-          res.status(400).send({ message: errorData })
-        } else {
-          res.status(200).send({ data: results })
-        }
-      }
-    )
-  } catch (error: AnyType) {
-    res.status(400).send({ message: error })
-  }
-}
-exports.getNivelAcademico = async (req: Request, res: Response) => {
-  try {
-    conexion.query(
-      'SELECT * FROM nivel_academico',
-      (err: AnyType, results: AnyType) => {
-        if (results?.length === 0) {
-          res.status(400).send({ message: errorData })
-        } else {
-          res.status(200).send({ data: results })
-        }
-      }
-    )
-  } catch (error: AnyType) {
-    res.status(400).send({ message: error })
-  }
-}
 
 exports.getConsultas = async (req: Request, res: Response) => {
   try {
@@ -139,52 +74,6 @@ exports.updateConsultas = async (req: Request, res: Response) => {
     }
   )
 }
-exports.getNomina = async (req: Request, res: Response) => {
-  try {
-    conexion.query('SELECT * FROM nomina', (err: AnyType, results: AnyType) => {
-      if (results?.length === 0) {
-        res.status(400).send({ message: errorData })
-      } else {
-        res.status(200).send({ data: results })
-      }
-    })
-  } catch (error: AnyType) {
-    res.status(400).send({ message: error })
-  }
-}
-exports.getTipoDocumentos = async (req: Request, res: Response) => {
-  try {
-    conexion.query(
-      'SELECT * FROM tipo_documentos',
-      (err: AnyType, results: AnyType) => {
-        if (results?.length === 0) {
-          res.status(400).send({ message: errorData })
-        } else {
-          res.status(200).send({ data: results })
-        }
-      }
-    )
-  } catch (error: AnyType) {
-    res.status(400).send({ message: error })
-  }
-}
-
-exports.getInfoEmpresa = async (req: Request, res: Response) => {
-  try {
-    conexion.query(
-      'SELECT * FROM empresa',
-      (err: AnyType, results: AnyType) => {
-        if (results?.length === 0) {
-          res.status(400).send({ message: errorData })
-        } else {
-          res.status(200).send({ data: results })
-        }
-      }
-    )
-  } catch (error: AnyType) {
-    res.status(400).send({ message: error })
-  }
-}
 exports.getDoctores = async (req: Request, res: Response) => {
   try {
     conexion.query(
@@ -201,121 +90,16 @@ exports.getDoctores = async (req: Request, res: Response) => {
     res.status(400).send({ message: error })
   }
 }
-exports.getCargo = async (req: Request, res: Response) => {
-  try {
-    conexion.query('SELECT * FROM cargos', (err: AnyType, results: AnyType) => {
-      if (results?.length === 0) {
-        res.status(400).send({ message: errorData })
-      } else {
-        res.status(200).send({ data: results })
-      }
-    })
-  } catch (error: AnyType) {
-    res.status(400).send({ message: error })
-  }
-}
-exports.getBloodType = async (req: Request, res: Response) => {
-  try {
-    conexion.query(
-      'SELECT * FROM tipos_sangre',
-      (err: AnyType, results: AnyType) => {
-        if (results?.length === 0) {
-          res.status(400).send({ message: errorData })
-        } else {
-          res.status(200).send({ data: results })
-        }
-      }
-    )
-  } catch (error: AnyType) {
-    res.status(400).send({ message: error })
-  }
-}
-exports.getParentesco = async (req: Request, res: Response) => {
-  try {
-    conexion.query(
-      'SELECT * FROM parentesco',
-      (err: AnyType, results: AnyType) => {
-        if (results?.length === 0) {
-          res.status(400).send({ message: errorData })
-        } else {
-          res.status(200).send({ data: results })
-        }
-      }
-    )
-  } catch (error: AnyType) {
-    res.status(400).send({ message: error })
-  }
-}
-exports.getTipoTelefono = async (req: Request, res: Response) => {
-  try {
-    conexion.query(
-      'SELECT * FROM tipos_telefono',
-      (err: AnyType, results: AnyType) => {
-        if (results?.length === 0) {
-          res.status(400).send({ message: errorData })
-        } else {
-          res.status(200).send({ data: results })
-        }
-      }
-    )
-  } catch (error: AnyType) {
-    res.status(400).send({ message: error })
-  }
-}
 
 exports.getPaises = async (req: Request, res: Response) => {
   try {
-    conexion.query('SELECT * FROM paises', (err: AnyType, results: AnyType) => {
-      if (results?.length === 0) {
-        res.status(400).send({ message: errorData })
-      } else {
-        res.status(200).send({ data: results })
-      }
-    })
-  } catch (error: AnyType) {
-    res.status(400).send({ message: error })
-  }
-}
-
-exports.getProvincias = async (req: Request, res: Response) => {
-  const { id_pais } = await new provincias(req.body.condition)
-
-  try {
     conexion.query(
-      id_pais === undefined
-        ? `SELECT * FROM provincias`
-        : `SELECT * FROM provincias WHERE id_pais = ?`,
-      [id_pais],
+      'SELECT * FROM nacionalidad',
       (err: AnyType, results: AnyType) => {
         if (results?.length === 0) {
           res.status(400).send({ message: errorData })
         } else {
           res.status(200).send({ data: results })
-        }
-      }
-    )
-  } catch (error: AnyType) {
-    res.status(400).send({ message: error })
-  }
-}
-
-exports.getParametros = async (req: Request, res: Response) => {
-  const { id_actividad } = await new parametros(req.body.condition)
-
-  try {
-    conexion.query(
-      `SELECT * FROM parametros WHERE id_actividad = ? `,
-      [id_actividad],
-      (err: AnyType, results: AnyType) => {
-        if (results?.length === 0) {
-          res.status(400).send({ message: errorData })
-        } else {
-          let ArrayParametros = results.map((item: any) => {
-            return { [item.parametro]: item.valor }
-          }, [])
-          res.status(200).send({
-            data: unirArrayEnObjeto(ArrayParametros),
-          })
         }
       }
     )
--- entity/general.ts
+++ entity/general.ts
@@ -1,56 +1,5 @@
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  PrimaryColumn,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm'
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 import BaseEntity from './BaseEntity'
-
-@Entity('PERSONAS')
-export class Personas extends BaseEntity<Personas> {
-  @Column({ type: 'varchar2', length: 13, nullable: true })
-  doc_identidad: string
-}
-@Entity('Empleado')
-export class Empleado extends BaseEntity<Empleado> {
-  @Column({ type: 'number', nullable: true })
-  id_empleado: number
-  @Column({ type: 'varchar2', nullable: true })
-  estado: string
-}
-@Entity('FILTER_STATE')
-export class filter_state extends BaseEntity<filter_state> {
-  @Column({ type: 'varchar2', nullable: true })
-  search: string
-  @Column({ type: 'varchar2', nullable: true })
-  type: string
-}
-@Entity('PROVINCIAS')
-export class provincias extends BaseEntity<provincias> {
-  @Column({ type: 'number', nullable: true })
-  id_pais: number
-}
-@Entity('PARAMETROS')
-export class parametros extends BaseEntity<provincias> {
-  @Column({ type: 'varchar', nullable: true })
-  id_actividad: string
-}
-@Entity('NOMINA')
-export class Nomina extends BaseEntity<Nomina> {
-  @Column({ type: 'varchar', nullable: true })
-  tipo: string
-}
-@Entity('TIPO_PERMISOS')
-export class getTiposPermisos extends BaseEntity<getTiposPermisos> {
-  @PrimaryGeneratedColumn()
-  id: number
-  @Column({ type: 'varchar', nullable: true })
-  tipo_permiso: string
-  @Column({ type: 'varchar', nullable: true })
-  estado: string
-}
 @Entity('CONSULTAS')
 export class Consultas extends BaseEntity<Consultas> {
   @PrimaryGeneratedColumn()
